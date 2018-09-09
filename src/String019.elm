ad7de9a5d02dc8504043052d507d8a6573481b97
Ryan Rempel <rgrempel@gmail.com>
2018-09-08 23:00:33 -0500

    Implement Elm 0.19's `toInt` and `toFloat`

--- src/String019.elm
+++ src/String019.elm
@@ -1,19 +1,25 @@
-module String019 exposing (replace, fromInt, fromFloat)
+module String019 exposing
+    ( replace, fromInt, fromFloat
+    , toInt, toFloat
+    )
 
-{-| Elm 0.19 added several new functions.
+{-| Elm 0.19 added several new functions, and changed the
+signature for `toInt` and `toFloat`.
 
 @docs replace, fromInt, fromFloat
+@docs toInt, toFloat
 
 -}
 
-import String exposing (split, join)
+import Result exposing (toMaybe)
+import String exposing (join, split)
 
 
 {-| Replace all occurrences of some substring.
 
     replace "." "-" "Json.Decode.succeed" --> "Json-Decode-succeed"
 
-    replace "," "/" "a,b,c,d,e"           --> "a/b/c/d/e"
+    replace "," "/" "a,b,c,d,e" --> "a/b/c/d/e"
 
 **Note:** If you need more advanced replacements, check out the
 [`elm/parser`][parser] or [`elm/regex`][regex] package.
@@ -33,7 +39,7 @@ replace before after string =
 
     fromInt -42 --> "-42"
 
-Check out [`Debug.toString`](Debug#toString) to convert *any* value to a string
+Check out [`Debug.toString`](Debug#toString) to convert _any_ value to a string
 for debugging purposes.
 
 -}
@@ -42,18 +48,64 @@ fromInt =
     toString
 
 
+{-| Try to convert a string into an int, failing on improperly formatted strings.
+
+    String019.toInt "123" --> Just 123
+
+    String019.toInt "-42" --> Just -42
+
+    String019.toInt "3.1" --> Nothing
+
+    String019.toInt "31a" --> Nothing
+
+If you are extracting a number from some raw user input, you will typically
+want to use [`Maybe.withDefault`](Maybe#withDefault) to handle bad data:
+
+    Maybe.withDefault 0 (String019.toInt "42") --> 42
+
+    Maybe.withDefault 0 (String019.toInt "ab") --> 0
+
+-}
+toInt : String -> Maybe Int
+toInt =
+    String.toInt >> toMaybe
+
+
 {-| Convert a `Float` to a `String`.
 
     fromFloat 123 --> "123"
 
     fromFloat -42 --> "-42"
 
-fromFloat 3.9 --> "3.9"
+    fromFloat 3.9 --> "3.9"
 
-Check out [`Debug.toString`](Debug#toString) to convert *any* value to a string
+Check out [`Debug.toString`](Debug#toString) to convert _any_ value to a string
 for debugging purposes.
 
 -}
 fromFloat : Float -> String
 fromFloat =
     toString
+
+
+{-| Try to convert a string into a float, failing on improperly formatted strings.
+
+    String019.toFloat "123" --> Just 123.0
+
+    String019.toFloat "-42" --> Just -42.0
+
+    String019.toFloat "3.1" --> Just 3.1
+
+    String019.toFloat "31a" --> Nothing
+
+If you are extracting a number from some raw user input, you will typically
+want to use [`Maybe.withDefault`](Maybe#withDefault) to handle bad data:
+
+    Maybe.withDefault 0 (String019.toFloat "42.5") --> 42.5
+
+    Maybe.withDefault 0 (String019.toFloat "cats") --> 0
+
+-}
+toFloat : String -> Maybe Float
+toFloat =
+    String.toFloat >> toMaybe
